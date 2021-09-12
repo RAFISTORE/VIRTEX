13601a2364c78c30c2169a8b2ed3793d761f31bb
RAFISTORE <90547663+RAFISTORE@users.noreply.github.com>
2021-09-12 15:16:31 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# VIRTEX
+# virtexV2
+Virtex Whatsapp V2
+
+Untuk cara instalasi $ menggunakan ya silahkan cek di Channel Youtube Saya (AsecC eror44)
---
+++ setup.sh
@@ -0,0 +1,12 @@
+clear
+echo
+apt update && apt upgrade
+apt install ruby
+apt install curl
+apt install figlet
+gem install lolcat
+termux-setup-storage
+cd $HOME/storage/shared
+mkdir virtex
+cd $HOME/virtexV2
+echo
